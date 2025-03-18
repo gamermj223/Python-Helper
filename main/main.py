720cf1868e6b32079ba6d9b234dda9f46090dfee
gamermj223 <90112379+gamermj223@users.noreply.github.com>
2025-03-18 15:42:50 -0400

    .

--- main/main.py
+++ main/main.py
@@ -1,2 +1,23 @@
-user = input("You: ")
-print(user)
+import time
+import random
+
+username = "You"
+ai_name = "Bob"
+when_sending_message = ": "
+active = True
+user_asked_exit = False
+
+def exit():
+    global user_asked_exit
+    if user == "exit":
+        user_asked_exit = True
+        print(ai_name+when_sending_message+"Are you sure?")
+    
+    if user_asked_exit == True:
+        active = False
+
+while active == True:
+
+    user = input(username+when_sending_message)
+
+    exit()
